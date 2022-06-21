bc655ef51ceb29b4cd981e43dc0d659446c4e815
Christina Loiacono <loiaconochristina@gmail.com>
2022-06-20 21:42:00 -0400

    removed bright red borders, updated text on home page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,27 +1,20 @@
-// import SingleTickerBTC from "./Widgets/SingleTickerBTC";
-// import CryptoMarket from "./Widgets/CryptoMarket";
 import SymbolOverview from "./Widgets/SymbolOverview";
 import "./Home.css";
 
 function Home() {
-  
   return (
     <div id="chartContainer">
       <header>
         <h1 className="fade-in-image">Welcome to CryptoTalk</h1>
-        <h4 className="">Your education portal for crypto</h4>
+        <h4 className="">Talk Crypto To Me</h4>
       </header>
       <div id="chart-coins-container">
         <div id="chart-widget">
-          <h4>CRYPTO CHARTS</h4>
-          {/* <SingleTickerBTC /> */}
           <SymbolOverview />
-          {/* <div id="chart">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Pushkin_population_history.svg/1200px-Pushkin_population_history.svg.png"
-              alt="chart"
-            />
-          </div> */}
+          <div className="tradingview-widget-container">
+            <div id="tradingview_0c358"> </div>
+          </div>
+          <h1 className="say-hi">Say hello to Alan, your guide &#8594;</h1>
         </div>
       </div>
     </div>
