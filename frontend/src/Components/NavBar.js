a7598c73b184367e208dfce44b3eecf067054ee3
Christina Loiacono <loiaconochristina@gmail.com>
2022-06-26 08:47:28 -0400

    deleted NavBar js and css. Created NavBar js and css again, without Material UI

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,50 +1,56 @@
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
-import logo from "../Images/chatbot-logo.png";
+import logo from "../assets/cryptotalk-logo.png"
 
 function NavBar() {
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <div>
-      <div>
-        <img id="navbar-logo" src={logo} alt="cryptotalk logo" />
+    <div className="navbar">
+
+      <div className="navbar-logo-container">
+        <Link to="/">
+
+          <div className="navbar-logo-and-text">
+
+            <div className="nav-app-logo">
+              <img src={logo} alt="cryptotalk logo" />
+            </div>
+            
+            <div className="nav-app-name">
+              CryptoTalk
+              <div className="nav-app-tag">
+                Talk Crypto To Me 
+              </div>
+            </div>
+
+          </div>
+
+        </Link>
       </div>
-      <Link to="/articles"></Link>
-      <Link to="/videos">Videos</Link>
-      <Link to="/news">News</Link>
+
+      <div className="navbar-links-container">
+
+        <div className="navbar-links">
+          <div><Link to="/cryptocurrency">Cryptocurrency</Link></div>
+          <div><Link to="/articles">Articles</Link></div>
+          <div><Link to="/videos">Videos</Link></div>
+          <div><Link to="/news">News</Link></div>
+          
+          <div className="navlinks">
+            <div class="dropdown">
+              <div class="dropbtn"><Link to="/users">Users</Link></div>
+              <div class="dropdown-content">
+                <Link to="/users/login">Users/Login</Link>
+                <Link to="/users/new">Users/New "Sign Up"</Link>
+                <Link to="/users/:id/edit">Users/:ID/Edit</Link>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default NavBar;
