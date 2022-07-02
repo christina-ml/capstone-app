110c5d9c6715be6f98c70805a4ef74a18b9fe87a
Christina Loiacono <loiaconochristina@gmail.com>
2022-07-02 13:42:52 -0400

    moved designUtils folder, added pageNotFound text and scss

--- frontend/src/Components/PageNotFound.js
+++ frontend/src/Components/PageNotFound.js
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
 
+import '../App.scss';
+
+// react-icons
+import { SiProbot } from 'react-icons/si';
+
 function PageNotFound() {
     return (
-      <div className="page-not-found">
-        <h1> Page Not found</h1>
-        <Link to="/"><h1>Back Home</h1></Link>
+      <div className="pageNotFound">
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <div className="pageNotFound__robot">
+          <SiProbot />
+        </div>
+        <div className="pageNotFound__backButton">
+          <Link to="/">
+            <h2>Back to CryptoTalk</h2>
+          </Link>
+        </div>
       </div>
     );
   }
