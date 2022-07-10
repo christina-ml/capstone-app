223f1a5aba7b3a8dca10795408e3f023b3a8f037
Christina Loiacono <loiaconochristina@gmail.com>
2022-07-10 15:00:13 -0400

    backend - updated schema and seed file

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,39 +1,39 @@
-DROP DATABASE IF EXISTS crypto_users;
-CREATE DATABASE crypto_users;
+DROP DATABASE IF EXISTS crypto_dev;
+CREATE DATABASE crypto_dev;
 
-\c crypto_users;
+\c crypto_dev;
 
 CREATE TABLE users(
-  id SERIAL PRIMARY KEY,
+  uid SERIAL PRIMARY KEY,
   firstname TEXT,
   lastname TEXT,
   username TEXT UNIQUE NOT NULL,
-  password TEXT,
-  contact_email TEXT,
-  active BOOLEAN
+  user_password TEXT,
+  user_email TEXT,
+  user_active BOOLEAN,
+  user_interests TEXT [],
+  user_city TEXT,
+  user_state TEXT
 );
 
 DROP TABLE IF EXISTS resources;
 
 CREATE TABLE resources(
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
-    type TEXT,
-    category TEXT,
-    url VARCHAR(2048) UNIQUE,
-    is_favorite BOOLEAN
+  rid SERIAL PRIMARY KEY,
+  resource_name TEXT NOT NULL UNIQUE,
+  resource_type TEXT,
+  resource_category TEXT,
+  resource_url VARCHAR(2048) UNIQUE
 );
 
 DROP TABLE IF EXISTS favorites;
 
 CREATE TABLE favorites(
-  id SERIAL PRIMARY KEY,
-  favorited_date TIMESTAMP ,
-  resources_id INTEGER REFERENCES resources(id)
-  ON DELETE CASCADE,
-  users_id INTEGER REFERENCES users(id)
-  ON DELETE CASCADE,
-  resources_name TEXT REFERENCES resources(name),
-  resources_url TEXT REFERENCES resources(url)
+  fid SERIAL PRIMARY KEY,
+  fav_date TIMESTAMP,
+  fav_user_id INTEGER REFERENCES users(uid) ON DELETE CASCADE,
+  fav_resource_id INTEGER REFERENCES resources(rid) ON DELETE CASCADE,
+  fav_resource_name TEXT REFERENCES resources(resource_name) ON DELETE CASCADE,
+  fav_resource_url TEXT REFERENCES resources(resource_url) ON DELETE CASCADE
 );
 
--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,133 +1,116 @@
-\c crypto_users;
+\c crypto_dev;
 
-INSERT INTO users(firstname, lastname, username, password, contact_email, active) VALUES
-('Christina', 'Loiacono', 'Christina_234','12345','123@gmail.org', true),
-('Oliver', 'Chuzan', 'Oliver_123','mypassword123','HTMLStudy@meetup.com', true),
-('Deborah', 'Campos', 'Deb2468','54321','1abc@gmail', true),
-('Milton', 'Clark', 'Milton456','mPassword345','Flexbox@studygroup.com', true),
-('Ruben', 'Profit', 'Ruben4321','yupYup2022','codepro@mentor.com', true),
-('Olaver', 'Cheezin', 'Olaver_123','mepass100','HTMLman@meetup.com', false),
-('Dab', 'Camper', 'Dab2468','543a210','ethereum@gmail.edu', false),
-('Malton', 'Clerk', 'Malton456','drowssaP','CSSlearner@study.com', false),
-('Greg', 'Coffeeman', 'coffee7Shop','cupOcoffee1','beans@coffee.com', true),
-('Sam', 'Caffeinated', 'g00dCoffee','goodCoffe3','garbagecoffee@coffee.com', true),
-('Cody', 'Coder', 'coderHero','codecode4','cody@coding.com', true);
+INSERT INTO users(firstname, lastname, username, user_password, user_email, user_active, user_interests, user_city, user_state) VALUES
+('Christina', 'Loiacono', 'rowboatoutraged30','PWm3dmVg','christina@coinboard.com', true, ARRAY [ 'bitcoin', 'solana' ], 'New York City', 'New York'),
+('Oliver', 'Chuzan', 'yawningkangaroo197','w74Mb7tQ','oliver@cryptoville.com', true, ARRAY [ 'bitcoin', 'tether' ], 'Toledo', 'Ohio'),
+('Deborah', 'Campos', 'peacenative441','gn5RhAyk','deb@coinreport.com', true, ARRAY [ 'litecoin', 'binance USD' ], 'Austin', 'Texas'),
+('Milton', 'Clark', 'desertloafers650','pRu7DF6Lxk','milton@mineville.com', true, ARRAY [ 'USD coin', 'dogecoin' ], 'Greensboro', 'North Carolina'),
+('Ruben', 'Profit', 'insecurepastry40','h7MVjYDQTq','rprofit@cryptodash.com', true, ARRAY [ 'dogecoin', 'ethereum' ], 'San Jose', 'California'),
+('Olivia', 'Figueroa', 'figuresneak10','x44jarGnXdH','olives@coinpixel.com', true, ARRAY [ 'ethereum', 'solana' ], 'Tampa', 'Florida'),
+('Casey', 'Hopkins', 'yikesoctopus31','P6YGheJTK','hopkinsc@cryptostyle.org', true, ARRAY [ 'binance', 'bitcoin' ], 'Santa Ana', 'California'),
+('Wilbur', 'Mendoza', 'admissionmeerkat75','7fg3H3cx4WpW','wilbur@cryptocooks.org', true, ARRAY [ 'ethereum', 'uniswap' ], 'Jersey City', 'New Jersey'),
+('Karla', 'Peterson', 'fieldsblessing381','4CzwYXk9p','karlapet@skipadoo.org', true, ARRAY [ 'USD coin', 'binance USD' ], 'Las Vegas', 'Nevada'),
+('Calvin', 'Leonard', 'violenthamburger175','3Cjv3DYJX2SB','calleon@cryptodesk.com', true, ARRAY [ 'solana', 'tether' ], 'Fort Wayne', 'Indiana'),
+('Lucy', 'Lee', 'papayadoves24','M95GCSmHe','lucylee@busycoin.org', true, ARRAY [ 'bitcoin', 'cardano' ], 'Phoenix', 'Arizona'),
+('Jesse', 'Becker', 'scoffdelivery11','LnMb8L8j23p','jess@coinsonic.com', true, ARRAY [ 'USD coin', 'ethereum' ], 'Detroit', 'Michigan'),
+('Ana', 'Knight', 'kitchenpudding21','f23MhW7xe','aknight@easymine.com', true, ARRAY [ 'binance', 'dogecoin' ], 'Milwaukee', 'Wisconsin'),
+('Kerry', 'Mckenzie', 'shawlpoke','cSt8jQsM','kerry@nerdcoin.org', true, ARRAY [ 'cardano', 'uniswap' ], 'Laredo', 'Texas' ),
+('Darrin', 'Reynolds', 'analyticallizard','vY2ZEt96','dreynolds@cryptoboard.edu', true, ARRAY [ 'bitcoin', 'tether' ], 'Baltimore', 'Maryland');
 
-INSERT INTO resources(name, type, category, url, is_favorite) VALUES
+INSERT INTO resources(resource_name, resource_type, resource_category, resource_url) VALUES
 (
     'Dodgecoin Jumps On Elon Musk Space X Tweet', 'article', 'news', 
-    'https://www.coindesk.com/business/2022/05/27/dogecoin-jumps-on-elon-musk-space-x-tweet/', 
-    true
+    'https://www.coindesk.com/business/2022/05/27/dogecoin-jumps-on-elon-musk-space-x-tweet/'
 ),
 (
     'Crypto City Guide To New York', 'article', 'news', 
-    'https://cointelegraph.com/magazine/2021/10/28/crypto-city-guide-to-new-york', 
-    true
+    'https://cointelegraph.com/magazine/2021/10/28/crypto-city-guide-to-new-york'
 ),
 (
     'Crypto Is Changing How Humanitarian Agencies Deliver Aid', 'article', 'news', 
-    'https://cointelegraph.com/magazine/2022/05/25/crypto-blockchain-changing-humanitarian-agencies-deliver-aid', 
-    true
+    'https://cointelegraph.com/magazine/2022/05/25/crypto-blockchain-changing-humanitarian-agencies-deliver-aid'
 ),
 (
     'How Can The U.S. Establish Itself As a Crypto Leader?', 'article', 'news', 
-    'https://www.coindesk.com/layer2/2022/05/27/how-the-us-can-establish-itself-as-a-crypto-leader/', 
-    true
+    'https://www.coindesk.com/layer2/2022/05/27/how-the-us-can-establish-itself-as-a-crypto-leader/'
 ),
 (
     'How To Keep Your Crypto Safe', 'article', 'news', 
-    'https://cointelegraph.com/magazine/2022/01/25/security-experts-reveal-how-keep-crypto-safe', 
-    true
+    'https://cointelegraph.com/magazine/2022/01/25/security-experts-reveal-how-keep-crypto-safe'
 ),
 (   
     'Inflationary & Deflationary Cryptocurrencies', 'article', 'news', 
-    'https://www.coindesk.com/learn/inflationary-and-deflationary-cryptocurrencies-whats-the-difference/', 
-    true
+    'https://www.coindesk.com/learn/inflationary-and-deflationary-cryptocurrencies-whats-the-difference/'
 ),
 (   
     'Stripe Partners With Opennnode To Allow Instant Bitcoin Conversions for Businesses', 'article', 'news', 
-    'https://www.coindesk.com/business/2022/05/24/stripe-partners-with-opennode-to-allow-instant-fiat-to-bitcoin-conversions-for-businesses/', 
-    true
+    'https://www.coindesk.com/business/2022/05/24/stripe-partners-with-opennode-to-allow-instant-fiat-to-bitcoin-conversions-for-businesses/'
 ),
 (   
     '6 Crypto Myths Debunked', 'article', 'news',
-    'https://www.gobankingrates.com/investing/crypto/crypto-myths-debunked/', 
-    true
+    'https://www.gobankingrates.com/investing/crypto/crypto-myths-debunked/'
 ),
 (   
     'How To Use Crypto For Buy Now Pay Later Transactions', 'article', 'news', 
-    'https://www.gobankingrates.com/investing/crypto/how-use-crypto-buy-now-pay-later-transactions/', 
-    true
+    'https://www.gobankingrates.com/investing/crypto/how-use-crypto-buy-now-pay-later-transactions/'
 ),
 (   
     'What is Bitcoin?', 'article', 'education-article', 
-    'https://www.bitcoin.com/get-started/what-is-bitcoin/', 
-    true
+    'https://www.bitcoin.com/get-started/what-is-bitcoin/'
 ),
 (   
     'How to Buy Bitcoin', 'article', 'education-article', 
-    'https://www.bitcoin.com/get-started/how-to-buy-bitcoin/', 
-    true
+    'https://www.bitcoin.com/get-started/how-to-buy-bitcoin/'
 ),
 (   
     'How to Sell Bitcoin', 'article', 'education-article', 
-    'https://www.bitcoin.com/get-started/how-to-sell-bitcoin/', 
-    true
+    'https://www.bitcoin.com/get-started/how-to-sell-bitcoin/'
 ),
 (   
     'What is a Bitcoin Wallet?', 'article', 'education-article', 
-    'https://www.bitcoin.com/get-started/what-is-a-bitcoin-wallet/', 
-    true
+    'https://www.bitcoin.com/get-started/what-is-a-bitcoin-wallet/'
 ),
 (   
     'What is Ethereum?', 'article', 'education-article', 
-    'https://www.bitcoin.com/get-started/what-is-ethereum/', 
-    true
+    'https://www.bitcoin.com/get-started/what-is-ethereum/'
 ),
 (   
     'What is Ethereum Used For?', 'article', 'education-article', 
-    'https://www.bitcoin.com/get-started/what-is-eth-used-for/', 
-    true
+    'https://www.bitcoin.com/get-started/what-is-eth-used-for/'
 ),
 (   
     'What is Bitcoin? Bitcoin Explained Simply', 'video', 'education', 
-    'https://www.youtube.com/embed/41JCpzvnn_0', 
-    true
+    'https://www.youtube.com/embed/41JCpzvnn_0'
 ),
 (   
     'Bitcoin Cryptocurrency For Beginners 2022', 'video', 'education', 
-    'https://www.youtube.com/embed/WO-O2K_Fqw0', 
-    true
+    'https://www.youtube.com/embed/WO-O2K_Fqw0'
 ),
 (   
     'How does a blockchain work - Simply Explained', 'video', 'education', 
-    'https://www.youtube.com/embed/SSo_EIwHSd4', 
-    true
+    'https://www.youtube.com/embed/SSo_EIwHSd4'
 ),
 (   
     'Cryptocurrency In 5 Minutes | Cryptocurrency Explained', 'video', 'education', 
-    'https://www.youtube.com/embed/1YyAzVmP9xQ', 
-    true
+    'https://www.youtube.com/embed/1YyAzVmP9xQ'
 ),
 (   
     'How Cryptocurrency ACTUALLY works', 'video', 'education', 
-    'https://www.youtube.com/embed/rYQgy8QDEBI', 
-    true
+    'https://www.youtube.com/embed/rYQgy8QDEBI'
 ),
 (   
     'Cryptocurrency For Beginners Full Course', 'video', 'education', 
-    'https://www.youtube.com/embed/dgjZ2fHg4y4', 
-    true
+    'https://www.youtube.com/embed/dgjZ2fHg4y4'
 );
 
-INSERT INTO favorites(favorited_date, resources_id, users_id, resources_name, resources_url) VALUES
-('10/22/2017 10:00', 1, 2, 'Dodgecoin Jumps On Elon Musk Space X Tweet','https://www.coindesk.com/business/2022/05/27/dogecoin-jumps-on-elon-musk-space-x-tweet/'),
-('09/24/2019 10:15', 2, 1, 'Crypto City Guide To New York', 'https://cointelegraph.com/magazine/2021/10/28/crypto-city-guide-to-new-york'),
-('07/18/2018 12:00', 3, 4, 'Crypto Is Changing How Humanitarian Agencies Deliver Aid', 'https://cointelegraph.com/magazine/2022/05/25/crypto-blockchain-changing-humanitarian-agencies-deliver-aid'),
-('11/21/2021 09:45', 4, 3, 'How Can The U.S. Establish Itself As a Crypto Leader?','https://www.coindesk.com/layer2/2022/05/27/how-the-us-can-establish-itself-as-a-crypto-leader/'),
-('02/13/2021 14:00', 5, 6, 'How To Keep Your Crypto Safe','https://cointelegraph.com/magazine/2022/01/25/security-experts-reveal-how-keep-crypto-safe'),
-('03/02/2017 09:30', 13, 1, 'What is a Bitcoin Wallet?','https://www.bitcoin.com/get-started/what-is-a-bitcoin-wallet/' ),
-('04/22/2016 14:00', 17, 4, 'Bitcoin Cryptocurrency For Beginners 2022', 'https://www.youtube.com/embed/WO-O2K_Fqw0'),
-('05/19/2016 18:00', 16, 1, 'What is Bitcoin? Bitcoin Explained Simply', 'https://www.youtube.com/embed/41JCpzvnn_0'),
-('06/25/2014 20:15', 21, 2, 'Cryptocurrency For Beginners Full Course','https://www.youtube.com/embed/dgjZ2fHg4y4'),
-('01/11/2022 09:00', 10, 5, 'What is Bitcoin?','https://www.bitcoin.com/get-started/what-is-bitcoin/' );
+INSERT INTO favorites(fav_date, fav_user_id, fav_resource_id, fav_resource_name, fav_resource_url) VALUES
+('10/22/2017 10:00', 2, 1, 'Dodgecoin Jumps On Elon Musk Space X Tweet','https://www.coindesk.com/business/2022/05/27/dogecoin-jumps-on-elon-musk-space-x-tweet/'),
+('09/24/2019 10:15', 1, 2, 'Crypto City Guide To New York', 'https://cointelegraph.com/magazine/2021/10/28/crypto-city-guide-to-new-york'),
+('07/18/2018 12:00', 4, 3, 'Crypto Is Changing How Humanitarian Agencies Deliver Aid', 'https://cointelegraph.com/magazine/2022/05/25/crypto-blockchain-changing-humanitarian-agencies-deliver-aid'),
+('11/21/2021 09:45', 3, 4, 'How Can The U.S. Establish Itself As a Crypto Leader?','https://www.coindesk.com/layer2/2022/05/27/how-the-us-can-establish-itself-as-a-crypto-leader/'),
+('02/13/2021 14:00', 6, 5, 'How To Keep Your Crypto Safe','https://cointelegraph.com/magazine/2022/01/25/security-experts-reveal-how-keep-crypto-safe'),
+('03/02/2017 09:30', 1, 13, 'What is a Bitcoin Wallet?','https://www.bitcoin.com/get-started/what-is-a-bitcoin-wallet/' ),
+('04/22/2016 14:00', 4, 17, 'Bitcoin Cryptocurrency For Beginners 2022', 'https://www.youtube.com/embed/WO-O2K_Fqw0'),
+('05/19/2016 18:00', 1, 16, 'What is Bitcoin? Bitcoin Explained Simply', 'https://www.youtube.com/embed/41JCpzvnn_0'),
+('06/25/2014 20:15', 2, 21, 'Cryptocurrency For Beginners Full Course','https://www.youtube.com/embed/dgjZ2fHg4y4'),
+('01/11/2022 09:00', 5, 10, 'What is Bitcoin?','https://www.bitcoin.com/get-started/what-is-bitcoin/');
