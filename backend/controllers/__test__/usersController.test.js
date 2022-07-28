175e1f0b704bcd04d9021580cadbcee230cb9fea
Christina Loiacono <loiaconochristina@gmail.com>
2022-07-28 15:59:42 -0400

    removed extra test on backend. added colors and updated navbar links

--- backend/controllers/__test__/usersController.test.js
+++ backend/controllers/__test__/usersController.test.js
@@ -11,15 +11,5 @@ describe('returns json data for all users', () => {
                 expect(response.body.length).toBe(15);
             })        
     })
-
-    it ('returns an array with an individual users interests', async () => {
-        await supertest(app).get("/users")
-        .expect(200)
-        .then((response) => {
-            // console.log(response.body[0].user_interests)
-            expect(response.body[0].user_interests).toBeInstanceOf(Array);
-            expect(response.body[0].user_interests.length).toBe(2);
-        })
-    })
 })
 
