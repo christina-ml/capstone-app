50f88962cbfa6bce74eb88b3b3c5d2d11bb60d40
Christina Loiacono <loiaconochristina@gmail.com>
2022-08-18 15:59:51 -0700

    updated scss for 404 page container

--- frontend/src/Components/PageNotFound.js
+++ frontend/src/Components/PageNotFound.js
@@ -7,7 +7,11 @@ import { SiProbot } from 'react-icons/si';
 
 function PageNotFound() {
     return (
+      
       <div className="pageNotFound">
+        <div className="pageNotFound__circle"></div>
+        <div className="pageNotFound__antenna"></div>
+        <div className="pageNotFound__container">
         <h1>404</h1>
         <h2>Page Not Found</h2>
         <div className="pageNotFound__robot">
@@ -18,6 +22,8 @@ function PageNotFound() {
             <h2>Back to CryptoTalk</h2>
           </Link>
         </div>
+      </div>
+      <div className="pageNotFound__footUnderline"></div>
       </div>
     );
   }
