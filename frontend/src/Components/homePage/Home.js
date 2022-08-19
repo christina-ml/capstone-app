ff0f7752a7bc729b1948c83899bb5f4e314ddd30
Christina Loiacono <loiaconochristina@gmail.com>
2022-08-19 15:56:03 -0400

    added SneakerNews homepage layout to home.js, added css styling

--- frontend/src/Components/homePage/Home.js
+++ frontend/src/Components/homePage/Home.js
@@ -10,6 +10,53 @@ function Home() {
         <h1 className="home__logo">Welcome</h1>
         <h4 className="home__tag">Talk Crypto To Me</h4>
       </header>
+
+      {/* SneakerNews */}
+      <div className="sneakerNewsContainer">
+      <div class="container">
+        <div class="leftSideOfPage">
+          <div class="largeSneaker">
+            <img
+              src="https://sneakernews.com/wp-content/uploads/2022/08/vantas.jpg"
+              alt="yeezy sneaker"
+            />
+          </div>
+          <div class="headerText">Return Of VANTA</div>
+          <div class="subHeaderText">Where To Buy The Yeezy Boost 700 v2</div>
+        </div>
+
+        <div class="rightSideOfPage">
+          <div class="sneakerContainer">
+            <div class="sneaker">
+              <img
+                src="https://sneakernews.com/wp-content/uploads/2022/08/jjjjound-kayano-store-llist.jpg"
+                alt="asics sneaker"
+              />
+            </div>
+            <div class="title">Store List: JJJJound ASICS GEL-Kayano 14</div>
+          </div>
+
+          <div class="sneakerContainer">
+            <div class="sneaker">
+              <img
+                src="https://sneakernews.com/wp-content/uploads/2022/08/badvunnhyblue.jpg"
+                alt="adidas sneaker"
+              />
+            </div>
+
+            <div class="title">Bad Bunny adidas Forums</div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+
+
+
+
+
+
+
       {/* <div id="chart-coins-container"> */}
         {/* <div id="chart-widget"> */}
           {/* <SymbolOverview /> */}
