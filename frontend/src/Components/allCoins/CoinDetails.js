62ce4c65faeb470d0134e70690b25ab824eae828
Christina Loiacono <loiaconochristina@gmail.com>
2022-09-24 14:45:47 -0400

    updated CoinDetails.js classNames and added scss styling for individual coin cards

--- frontend/src/Components/allCoins/CoinDetails.js
+++ frontend/src/Components/allCoins/CoinDetails.js
@@ -23,39 +23,51 @@ const CoinDetails = () => {
 
   return (
     <div className="coinDetails">
-        <h3>coin Name: {oneCoin.name}</h3>
-        <div>
-            cid: {oneCoin.cid}
-            coinmarketcap ID: {oneCoin.coinmarketcap_id}
+
+        <div className="coinDetails__coinCard">
+            <div className="coinDetails__coinCard__logo">
+                <img src={oneCoin.logo} alt="coin logo" />
+                <div className="coinDetails__coinCard__logo__coinName">
+                {oneCoin.name} ({oneCoin.symbol})
+                </div>
+            </div>
+            <div className="coinDetails__coinCard__details">
+                <div>
+                    cid: {oneCoin.cid}
+                </div>
+                <div>
+                    coinmarketcap ID: {oneCoin.coinmarketcap_id}
+                </div>
+                <div>
+                    slug: {oneCoin.slug}
+                </div>
+                <div>
+                    num market pairs: {oneCoin.num_market_pairs}
+                </div>
+                <div>
+                    date added: {oneCoin.date_added}
+                </div>
+                <div>
+                    max supply: {oneCoin.max_supply}
+                </div>
+                <div>
+                    circulating supply: {oneCoin.circulating_supply}
+                </div>
+                <div>
+                    total supply: {oneCoin.total_supply}
+                </div>
+                <div>
+                    cmc rank: {oneCoin.cmc_rank}
+                </div>
+                <div>
+                    coin uid: {oneCoin.coin_uid}
+                </div>
+                <div>
+                    tags: {oneCoin.tags}
+                </div>
+            </div>
         </div>
-        <div>
-            name: {oneCoin.name}
-            symbol: {oneCoin.symbol}
-        </div>
-        <div>
-            logo: <img src={oneCoin.logo} alt="coin logo" />
-        </div>
-        <div>
-            slug: {oneCoin.slug}
-            num market pairs: {oneCoin.num_market_pairs}
-            date added: {oneCoin.date_added}
-        </div>
-        <div className="coinDetails__tagsList">
-            tags: {oneCoin.tags}
-        </div>
-        <div>
-            max supply: {oneCoin.max_supply}
-        </div>
-        <div>
-            circulating supply: {oneCoin.circulating_supply}
-            total supply: {oneCoin.total_supply}
-        </div>
-        <div>
-            cmc rank: {oneCoin.cmc_rank}
-        </div>
-        <div>
-            coin uid: {oneCoin.coin_uid}
-        </div>
+
     </div>
   )
 }
