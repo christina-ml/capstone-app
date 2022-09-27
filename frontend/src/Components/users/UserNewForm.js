de890089c447c6cdd2e2146df1522814e76afde1
Christina Loiacono <loiaconochristina@gmail.com>
2022-09-26 22:20:37 -0400

    updated scss and js files for UserNewForm to match login form

--- frontend/src/Components/users/UserNewForm.js
+++ frontend/src/Components/users/UserNewForm.js
@@ -50,117 +50,120 @@ function UserNewForm() {
 
   return (
     <div className="UserNewForm">
-      <form className="UserNewForm__form" onSubmit={handleSubmit}>
-        <div className="UserNewForm__form__inner">
-          <h2>Sign Up</h2>
+      <div className="UserNewForm__container">
+        <form className="UserNewForm__container__form" onSubmit={handleSubmit}>
+          <div className="UserNewForm__container__form__inner">
+            <h2>Sign Up</h2>
+          </div>
+          <div className="UserNewForm__container__form__inner__fields">
+            <div className="UserNewForm__container__form__inner__fields__group">
+              <label htmlFor='firstname'>First Name: </label>
+              <input 
+                id="firstname"
+                value={user.firstname}
+                type="text"
+                onChange={handleTextChange}
+                placeholder="First Name"
+              />
+            </div>
+
+            <div className="UserNewForm__container__form__inner__fields__group">
+              <label htmlFor='lastname'>Last Name: </label>
+              <input 
+                id="lastname"
+                value={user.lastname}
+                type="text"
+                onChange={handleTextChange}
+                placeholder="Last Name"
+              />        
+            </div>
+
+            <div className="UserNewForm__container__form__inner__fields__group">
+              <label htmlFor='username'>Username: </label>
+              <input 
+                id="username"
+                value={user.username}
+                type="text"
+                onChange={handleTextChange}
+                placeholder="Username"
+              />
+            </div>
+
+            <div className="UserNewForm__container__form__inner__fields__group">
+              <label htmlFor='user_password'>Password: </label>
+              <input 
+                id="user_password"
+                value={user.user_password}
+                type="text"
+                onChange={handleTextChange}
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="UserNewForm__container__form__inner__fields__group">
+              <label htmlFor='user_email'>Email: </label>
+              <input 
+                id="user_email"
+                value={user.user_email}
+                type="text"
+                onChange={handleTextChange}
+                placeholder="User Email"
+              />
+            </div>
+
+            <div className="UserNewForm__container__form__inner__fields__group">
+              <label htmlFor='user_interests'>Interests: </label>
+              <input 
+                id="user_interests"
+                value={user.user_interests}
+                type="text"
+                onChange={handleTextChange}
+                placeholder="User Interests"
+              />
+            </div>
+
+            <div className="UserNewForm__container__form__inner__fields__group">
+              <label htmlFor='user_city'>City: </label>
+              <input 
+                id="user_city"
+                value={user.user_city}
+                type="text"
+                onChange={handleTextChange}
+                placeholder="User City"
+              />
+            </div>
+
+            <div className="UserNewForm__container__form__inner__fields__group">
+              <label htmlFor='user_state'>State: </label>
+              <input 
+                id="user_state"
+                value={user.user_state}
+                type="text"
+                onChange={handleTextChange}
+                placeholder="User State"
+              />
+            </div>
+
+            <div className="UserNewForm__container__form__inner__fields__groupAdmin">
+              <label htmlFor='user_admin'>Admin: </label>
+              <input 
+                id="user_admin"
+                checked={user.user_admin}
+                type="checkbox"
+                onChange={handleCheckboxChange}
+              />
+            </div>
+
+          </div>
+          <br />
+          <input type="submit" />
+        </form>
+        <div className="UserNewForm__container__backToLogin">
+          Already a member? &nbsp;
+          <Link to="/login" >
+            Log In
+          </Link>
         </div>
-        <div className="UserNewForm__form__inner__fields">
-          <div className="UserNewForm__form__inner__fields__group">
-            <label htmlFor='firstname'>First Name:</label>
-            <input 
-              id="firstname"
-              value={user.firstname}
-              type="text"
-              onChange={handleTextChange}
-              placeholder="First Name"
-            />
-          </div>
-
-          <div className="UserNewForm__form__inner__fields__group">
-            <label htmlFor='lastname'>Last Name:</label>
-            <input 
-              id="lastname"
-              value={user.lastname}
-              type="text"
-              onChange={handleTextChange}
-              placeholder="Last Name"
-            />        
-          </div>
-
-          <div className="UserNewForm__form__inner__fields__group">
-            <label htmlFor='username'>Username:</label>
-            <input 
-              id="username"
-              value={user.username}
-              type="text"
-              onChange={handleTextChange}
-              placeholder="Username"
-            />
-          </div>
-
-          <div className="UserNewForm__form__inner__fields__group">
-            <label htmlFor='user_password'>Password:</label>
-            <input 
-              id="user_password"
-              value={user.user_password}
-              type="text"
-              onChange={handleTextChange}
-              placeholder="Password"
-            />
-          </div>
-
-          <div className="UserNewForm__form__inner__fields__group">
-            <label htmlFor='user_email'>Email:</label>
-            <input 
-              id="user_email"
-              value={user.user_email}
-              type="text"
-              onChange={handleTextChange}
-              placeholder="User Email"
-            />
-          </div>
-
-          <div className="UserNewForm__form__inner__fields__group">
-            <label htmlFor='user_admin'>Admin?:</label>
-            <input 
-              id="user_admin"
-              checked={user.user_admin}
-              type="checkbox"
-              onChange={handleCheckboxChange}
-            />
-          </div>
-
-          <div className="UserNewForm__form__inner__fields__group">
-            <label htmlFor='user_interests'>Interests:</label>
-            <input 
-              id="user_interests"
-              value={user.user_interests}
-              type="text"
-              onChange={handleTextChange}
-              placeholder="User Interests"
-            />
-          </div>
-
-          <div className="UserNewForm__form__inner__fields__group">
-            <label htmlFor='user_city'>City:</label>
-            <input 
-              id="user_city"
-              value={user.user_city}
-              type="text"
-              onChange={handleTextChange}
-              placeholder="User City"
-            />
-          </div>
-
-          <div className="UserNewForm__form__inner__fields__group">
-            <label htmlFor='user_state'>State:</label>
-            <input 
-              id="user_state"
-              value={user.user_state}
-              type="text"
-              onChange={handleTextChange}
-              placeholder="User State"
-            />
-          </div>
-        </div>
-        <br />
-        <input type="submit" />
-      </form>
-      <div className="UserNewForm__backToLogin">
-        Already a member? 
-        <Link to="/login" >
-          Log In
-        </Link>
       </div>
     </div>
   )
