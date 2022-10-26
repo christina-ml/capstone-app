ef57f8976bc9a3806581f11332b9db610798d3ee
Christina Loiacono <loiaconochristina@gmail.com>
2022-10-26 17:13:31 -0400

    added Chart.js line chart for coin percentage changes on homepage. SCSS styling

--- frontend/src/Components/allCoins/Coin.js
+++ frontend/src/Components/allCoins/Coin.js
@@ -3,7 +3,14 @@ import { Link } from 'react-router-dom';
 
 import './CoinList.scss';
 
+// helpers - displaying price with commas
+import addCommas from '../../helpers/AddCommas';
+
+// chart js for coin changes
+import ChartJsCoin from './chartjs/ChartJsCoin';
+
 const Coin = ({coin}) => {
+
   return (
     <div className="coin">
       <Link to={`/coins/${coin.cid}`}> 
@@ -11,13 +18,17 @@ const Coin = ({coin}) => {
           <div className="coin__card__logo">
             <img src={coin.logo} alt="coin logo" />
             <div className="coin__card__logo__coinName">
-              {coin.name}
+              {coin.name} <span className="coin__card__logo__coinName__symbol">{coin.symbol}</span>
             </div>
           </div>
           <hr />
           <div className="coin__card__basicInfo">
-            <div>Price: ${coin.price}</div>
-            <div>24h% {coin.percent_change_24h}%</div>
+            <div className="coin__card__basicInfo__price">Price: ${addCommas(coin.price)}</div>
+            <div className="coin__card__basicInfo__price">Market Cap: ${addCommas(coin.market_cap)}</div>
+            
+            <ChartJsCoin coin={coin} />
+
+            <div>Last Updated: {new Date(coin.last_updated).toLocaleString('en-US')}</div>
           </div>
         </div>
       </Link>
