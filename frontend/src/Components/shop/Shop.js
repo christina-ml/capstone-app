05aaad2976812102ab788cbd50c71d9875de7280
Christina Loiacono <loiaconochristina@gmail.com>
2022-10-28 18:53:11 -0400

    updated shop card and scss, made scss responsive for different screen widths

--- frontend/src/Components/shop/Shop.js
+++ frontend/src/Components/shop/Shop.js
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import nftData from './nftData';
 
-// import bluenft from './shopImages/blue_nft.jpeg'
-// import greennft from './shopImages/green_nft.jpeg'
-
 import { MdShoppingCart } from "react-icons/md";
-import { AiFillCheckCircle } from "react-icons/ai";
-import { AiFillCloseCircle } from "react-icons/ai";
 
 // helpers - convert USD to ETH
 import convertUSDToETH from '../../helpers/convertUSDToETH.js';
@@ -15,53 +10,40 @@ import convertUSDToETH from '../../helpers/convertUSDToETH.js';
 import './Shop.scss';
 
 const Shop = () => {
+
+ // make `nftData` sorted alphabetically
+  let sortedNftData = nftData.sort((a, b) => {
+    return (a.itemName > b.itemName) ? 1 : -1;
+  })
+
+
   return (
-    <div className="nftCards">
-        {nftData.map((nft, index) => {
-            return (
-                <div className="nftCards__card">
-                    {/* <div className="nftCards__card__banner"> */}
-                        {/* {nft} */}
-                        {/* <span>D</span><span>Only at Dick's</span> */}
-                        {/* <span>D</span><span>New</span> */}
-                        {/* <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> */}
-                    {/* </div> */}
-
-                    <div className="nftCards__card__details">
-                    <div className="nftCards__card__details__image">
-                        <React.Fragment>
-                            {nft.image === true && nft.color === 'yellowgreen' ? <span><img src={nft.url} alt="nft" /></span> : ''}
-                            {nft.image === true && nft.color === 'deepskyblue' ? <span><img src={nft.url} alt="nft" /></span> : ''}
-                        </React.Fragment>
-                    </div>
-
-                        <div className="nftCards__card__details__itemName">{nft.itemName}</div>
-                        <div className="nftCards__card__details__price">
-                            <img src={nft.currencyLogo} alt="currency logo"/>
-                            {convertUSDToETH(nft.price)} {nft.currency} <span>(${nft.price} USD)</span>
+    <div className="nftShop">
+        <h1>Explore, collect, and sell NFTs</h1>
+        <div className="nftCards">
+            {sortedNftData.map((nft, index) => {
+                return (
+                    <div className="nftCards__card" key={nft+index}>
+                        <div className="nftCards__card__details">
+                            <div className="nftCards__card__details__image">
+                                <img src={nft.url} alt="nft" />
+                            </div>
+                            <div className="nftCards__card__details__itemName">{nft.itemName}</div>
+                            <div className="nftCards__card__details__price">
+                                <div className="nftCards__card__details__price__current">Current price</div>
+                                <img src={nft.currencyLogo} alt="currency logo"/>
+                                {convertUSDToETH(nft.price)} {nft.currency} <span>${nft.price}</span>
+                            </div>
                         </div>
-                        <div className="nftCards__card__details__availNearby">
-                            <React.Fragment>
-                                {nft.availability === 'yes' ? <span><AiFillCheckCircle color="green"/>Shipping Available</span>: <></>}
-                                {nft.availability === 'no' ? <span><AiFillCloseCircle color="red"/>Not Available to Ship</span>: <></>}
-                                {nft.availability === 'limited' ? <span><AiFillCheckCircle color="orange"/>Limited Stock to Ship</span>: <></>}
-                            </React.Fragment>
-                            <React.Fragment>
-                                {nft.nearby === 'yes' ? <span><AiFillCheckCircle color="green" />Free Ship to Store</span>: <></>}
-                                {nft.nearby === 'no' ?  <span><AiFillCloseCircle color="red"/>Not Available to Pickup</span>: <></>}
-                                {nft.nearby === 'maybe' ? <span><AiFillCheckCircle color="orange" />In Nearby Stores</span>: <></>}
-                            </React.Fragment>
+                        <div className="nftCards__cart">
+                            <Link to="/cart"><MdShoppingCart color="sienna"/> Add To Cart</Link>
                         </div>
                     </div>
-                    <div className="nftCards__cart">
-                        <Link to="/cart"><MdShoppingCart color="sienna"/> Add To Cart</Link>
-                    </div>
-                </div>
-            )
-  
-           
-        })}
-        
+                )
+    
+            
+            })}
+        </div>
     </div>
   )
 }
