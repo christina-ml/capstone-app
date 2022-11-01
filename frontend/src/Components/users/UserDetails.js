eeb983de501363a898dac532730225be40a45119
Christina Loiacono <loiaconochristina@gmail.com>
2022-11-01 15:48:48 -0400

    updated User Details profile info. Added scss styling, react icons, password hide/show button

--- frontend/src/Components/users/UserDetails.js
+++ frontend/src/Components/users/UserDetails.js
@@ -4,11 +4,18 @@ import { Link, useParams } from 'react-router-dom';
 
 import Currencies from '../currencies/Currencies';
 
+// react icons
+import { MdOutlineEmail } from 'react-icons/md';
+import { IoSettingsSharp, IoHomeOutline } from 'react-icons/io5';
+
+
 const API = process.env.REACT_APP_API_URL;
 
 const UserDetails = () => {
 
     const [user, setUser] = useState([]);
+    const [showPassword, setShowPassword] = useState(false);
+
     let { uid } = useParams();
 
     useEffect(() => {
@@ -20,14 +27,17 @@ const UserDetails = () => {
             })
     })
 
-    const { firstname, lastname, username, user_password, user_email, user_admin, user_interests, user_city, user_state } = user;
+    const { firstname, lastname, username, user_password, user_email, user_admin, user_interests, user_city, user_state, photo } = user;
 
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
 
     return (
     <div className="userDetails">
         <div className="userDetails__editButton">
         <Link to={`/users/${user.uid}/edit`}>
-            <button>Edit User</button>
+            <button>Edit</button>
         </Link>
         </div>
         <div className="userDetails__details">
@@ -35,34 +45,38 @@ const UserDetails = () => {
                 <div>{firstname} {lastname}</div>
             </div>
             <div className="userDetails__details__bio">
-                <div>
-                    First Name: {firstname}
-                    <br />
-                    Last Name: {lastname}
+                <div className="userDetails__details__bio__profilePhoto">
+                    <img src={photo} alt="profile photo" />
                 </div>
                 <div>
-                    ID: {uid}
+                    {firstname} {lastname}
+                    {user_admin ? <span> &nbsp; <IoSettingsSharp /> Admin</span> : <span></span>}
                 </div>
                 <div>
-                    Username: {username}
+                    {username}
                 </div>
                 <div>
-                    Password: {user_password}
+                     
+                    { showPassword ? 
+                        <span> {user_password} </span> :
+                        <span> ********** </span>
+                    }
+                    { showPassword ? 
+                        <button onClick={handleShowPassword}>hide password</button> :
+                        <button onClick={handleShowPassword}>show password</button>
+                    }
                 </div>
                 <div>
-                    Admin? {user_admin ? <span>Yes</span> : <span>No</span>}
+                    CryptoTalk ID #: {user.uid}
+                </div>
+                <div>
+                    <MdOutlineEmail /> {user_email}
+                </div>
+                <div>
+                    <IoHomeOutline /> {user_city}, {user_state}
                 </div>
                 <div>
                     Interests: {user_interests}
-                </div>
-                <div>
-                    City: {user_city}
-                </div>
-                <div>
-                    State: {user_state}
-                </div>
-                <div>
-                    Email: {user_email}
                 </div>
             </div>
         </div>
