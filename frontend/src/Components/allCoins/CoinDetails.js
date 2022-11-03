91cf3367336ff5211fa04ee33b13e352e0e3883b
Christina Loiacono <loiaconochristina@gmail.com>
2022-11-02 23:20:15 -0400

    Adding CoinDetails information & scss styling layout, sections, data formatting, and colors

--- frontend/src/Components/allCoins/CoinDetails.js
+++ frontend/src/Components/allCoins/CoinDetails.js
@@ -7,6 +7,9 @@ import './CoinList.scss';
 // helpers - displaying price with commas
 import addCommas from '../../helpers/AddCommas';
 
+import ChartJsCoin from './chartjs/ChartJsCoin.js';
+
+
 const API = process.env.REACT_APP_API_URL;
 
 const CoinDetails = () => {
@@ -29,56 +32,106 @@ const CoinDetails = () => {
             <div className="coinDetails__coinCard__logo">
                 <img src={oneCoin.logo} alt="coin logo" />
                 <div className="coinDetails__coinCard__logo__coinName">
-                {oneCoin.name} ({oneCoin.symbol}) {oneCoin.slug}
+                {oneCoin.name} <span>{oneCoin.symbol}</span>
                 </div>
             </div>
+
             <div className="coinDetails__coinCard__details">
-                <div>
-                    date added: {new Date(oneCoin.date_added).toLocaleString('en-US')}
+                <div className="coinDetails__coinCard__details__oneAndTwo">
+                    {/* Section #1 */}
+                    <div>
+                        <div>
+                            Rank #{oneCoin.cmc_rank}
+                        </div>
+                    </div>
+
+                    {/* Section #2 */}
+                    <div>
+                        {oneCoin.slug} price ({oneCoin.symbol})
+                        <br />
+                        <div className="coinDetails__coinCard__details__oneAndTwo__price">
+                            ${addCommas(oneCoin.price)}
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    max supply: {addCommas(oneCoin.max_supply)}
-                    <br />
-                    circulating supply: {addCommas(oneCoin.circulating_supply)}
-                    <br />
-                    total supply: {addCommas(oneCoin.total_supply)}
+
+                {/* Section #3 */}
+                <div className="coinDetails__coinCard__details__sectionThree">
+                    <div>
+                        <div className="coinDetails__coinCard__details__sectionThree__label">
+                            Market Cap:
+                        </div>
+                        <div>${addCommas(oneCoin.market_cap)}</div>
+                        
+                    </div>
+                    <div>
+                        <div className="coinDetails__coinCard__details__sectionThree__label">
+                            Fully Diluted Market Cap:
+                        </div>
+                        <div>${addCommas(oneCoin.fully_diluted_market_cap)}</div>
+                    </div>
+                    <div>
+                        <div className="coinDetails__coinCard__details__sectionThree__label">
+                            Volume 24h:
+                        </div>
+                        <div>${addCommas(oneCoin.volume_24h)}</div>
+                        <div>volume change 24h: {addCommas(oneCoin.volume_change_24h)}</div>
+                    </div>
+                    <div>
+                        <div className="coinDetails__coinCard__details__sectionThree__label">
+                            Circulating Supply
+                        </div>
+                        <div>{addCommas(oneCoin.circulating_supply)} {oneCoin.symbol}</div>
+                        <div className="coinDetails__coinCard__details__sectionThree__label">
+                            Max Supply
+                        </div>
+                        <div>
+                            {(oneCoin.max_supply > 0) ? addCommas(oneCoin.max_supply) : <span>--</span>}
+                        </div>
+                        <div className="coinDetails__coinCard__details__sectionThree__label">
+                            Total Supply
+                        </div>
+                        <div>
+                            {addCommas(oneCoin.total_supply)}
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <div>Volume(24h): ${addCommas(oneCoin.volume_24h)}</div>
-                    <div>Circulating Supply: {addCommas(oneCoin.circulating_supply)} {oneCoin.symbol}</div>
+
+                {/* Section 4 */}
+                <div className="coinDetails__coinCard__details__sectionFour">
+                    <ChartJsCoin coin={oneCoin} />
+                    <div>
+                        Last updated: {new Date(oneCoin.last_updated).toLocaleString('en-US')}
+                    </div>
                 </div>
-                <div>
-                    num market pairs {oneCoin.num_market_pairs}
-                    <br />
-                    tags {oneCoin.tags}
-                    <br />
-                    token address {oneCoin.token_address}
-                    <br />
-                    cmc rank {oneCoin.cmc_rank}
-                    <br />
-                    self reported circulating supply {addCommas(oneCoin.self_reported_circulating_supply)}
-                    <br />
-                    self reported market cap {addCommas(oneCoin.self_reported_market_cap)}
-                    <br />
-                    tvl ratio {addCommas(oneCoin.tvl_ratio)}
-                    <br />
-                    last updated: {new Date(oneCoin.last_updated).toLocaleString('en-US')}
-                    <br />
-                    price: {addCommas(oneCoin.price)}
-                    <br />
-                    volume 24h {addCommas(oneCoin.volume_24h)}
-                    <br />
-                    volume change 24h {addCommas(oneCoin.volume_change_24h)}
-                    <br />
-                    market cap {addCommas(oneCoin.market_cap)}
-                    <br />
-                    market cap dominance {addCommas(oneCoin.market_cap_dominance)}
-                    <br />
-                    fully diluted market cap {addCommas(oneCoin.fully_diluted_market_cap)}
-                    <br />
-                    tvl {addCommas(oneCoin.tvl)}
-                </div>
+
             </div>
+
+
+                       
+            {/* <div>
+                date added: {new Date(oneCoin.date_added).toLocaleString('en-US')}
+                <br />
+                num market pairs {oneCoin.num_market_pairs}
+                <br />
+                tags {oneCoin.tags}
+                <br />
+                token address {oneCoin.token_address}
+                <br />
+                self reported circulating supply {addCommas(oneCoin.self_reported_circulating_supply)}
+                <br />
+                self reported market cap {addCommas(oneCoin.self_reported_market_cap)}
+                <br />
+                tvl ratio {addCommas(oneCoin.tvl_ratio)}
+                <br />
+                <br />
+                market cap dominance {addCommas(oneCoin.market_cap_dominance)}
+                <br />
+                tvl {addCommas(oneCoin.tvl)}
+            </div> */}
+            
+            
+
         </div>
     </div>
   )
