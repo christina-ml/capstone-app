36d3ecd6db96a4619d1ff274db66bf1d4613aa22
Christina Loiacono <loiaconochristina@gmail.com>
2022-11-05 14:46:33 -0400

    fixed image height bug on home page

--- frontend/src/Components/homePage/Home.js
+++ frontend/src/Components/homePage/Home.js
@@ -10,47 +10,47 @@ function Home() {
   return (
     <div className="home">
       <div className="home__container">
-        <div className="home__container__leftSideOfPage">
+        <div className="home__container__leftSide">
           <Link to="/learn">
-            <div className="home__container__leftSideOfPage__largeSneaker">
+            <div className="home__container__leftSide__largeImage">
               <img
                 src="https://cloudfront-us-east-2.images.arcpublishing.com/reuters/TK5SKXCQMNPBFOQOZZTJB55GHQ.jpg"
                 alt="get started with crypto"
               />
             </div>
-            <div className="home__container__leftSideOfPage__headerText">
+            <div className="home__container__leftSide__headerText">
               Get Started with Crypto
             </div>
-            <div className="home__container__leftSideOfPage__subHeaderText">
+            <div className="home__container__leftSide__subHeaderText">
               Start Learning About Bitcoin, Ethereum, Litecoin and more. NOW!
             </div>
           </Link>
         </div>
 
-        <div className="home__container__rightSideOfPage">
+        <div className="home__container__rightSide">
           <Link to="/news">
-            <div className="home__container__rightSideOfPage__sneakerContainer">
-              <div className="home__container__rightSideOfPage__sneakerContainer__sneaker">
+            <div className="home__container__rightSide__boxes">
+              <div className="home__container__rightSide__boxes__sideImage">
                 <img
                   src="https://penntoday.upenn.edu/sites/default/files/2022-01/cryptocurrency-main_1.jpg"
                   alt="crypto news"
                 />
               </div>
-              <div className="home__container__rightSideOfPage__sneakerContainer__title">
+              <div className="home__container__rightSide__boxes__title">
                 News: The Return of CRYPTO
               </div>
             </div>
           </Link>
 
           <Link to="/games">
-            <div className="home__container__rightSideOfPage__sneakerContainer">
-              <div className="home__container__rightSideOfPage__sneakerContainer__sneaker">
+            <div className="home__container__rightSide__boxes">
+              <div className="home__container__rightSide__boxes__sideImage">
                 <img
                   src="https://images.mktw.net/im-429485?width=700&height=487"
                   alt="crypto games"
                 />
               </div>
-              <div className="home__container__rightSideOfPage__sneakerContainer__title">
+              <div className="home__container__rightSide__boxes__title">
                 Crypto Games - Coloring and Matching
               </div>
             </div>
@@ -59,16 +59,6 @@ function Home() {
       </div>
 
       <CoinList />
-
-      {/* <div id="chart-coins-container"> */}
-      {/* <div id="chart-widget"> */}
-      {/* <SymbolOverview /> */}
-      {/* <div className="tradingview-widget-container">
-            <div id="tradingview_0c358"> </div>
-          </div> */}
-      {/* <h1 className="say-hi">Say hello to Alan, your guide &#8594;</h1> */}
-      {/* </div> */}
-      {/* </div> */}
     </div>
   );
 }
