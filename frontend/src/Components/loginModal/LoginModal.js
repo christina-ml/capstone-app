d371fd69e3df8a77722f462a8d0a67c1e0d1f714
Christina Loiacono <loiaconochristina@gmail.com>
2023-01-26 12:41:58 -0500

    switched Box to Stack layout for LoginModal. Added TextFields for Create an Account

--- frontend/src/Components/loginModal/LoginModal.js
+++ frontend/src/Components/loginModal/LoginModal.js
@@ -5,6 +5,8 @@ import './LoginModal.scss';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
+import Stack from '@mui/material/Stack';
 
 /**
  * Login Modal is not going to have a route - it will be available on every page
@@ -23,12 +25,19 @@ const LoginModal = ({ openLoginModal, setOpenLoginModal }) => {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 400,
+        // width: 400,
         bgcolor: 'background.paper',
         border: '2px solid #000',
         boxShadow: 24,
         p: 4,
+        width: '25ch'
     };
+
+    // firstname
+    // lastname
+    // username
+    // email
+    // password
 
     // hooks
     // const [username, setUsername] = useState('');
@@ -73,23 +82,30 @@ const LoginModal = ({ openLoginModal, setOpenLoginModal }) => {
     // }
 
     return (
-        <div className="LoginModal">
-        <Modal
-            open={openLoginModal}
+        <Modal className="LoginModal"
+            open={openLoginModal} // what we want to use (from state)
+            // open={true} // keeping open while creating text fields/scss
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
-            <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Text in a modal
+            <Stack
+                component="form"
+                sx={style}
+                spacing={2}
+                noValidate
+                autoComplete="off"
+            >
+                <Typography className="LoginModal__title" id="modal-modal-title" variant="h6" component="h2">
+                    Please Create An Account
                 </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Lorem ipsum Lorem ipsum Lorem ipsum...
-                </Typography>
-            </Box>
+                <TextField id="outlined-basic" label="First Name" variant="outlined" />
+                <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+                <TextField id="outlined-basic" label="Username" variant="outlined" required />
+                <TextField id="outlined-basic" label="Email" variant="outlined" required />
+                <TextField id="outlined-basic" label="Password" variant="outlined" required />
+            </Stack>
         </Modal>
-        </div>
     )
 }
 
