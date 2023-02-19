df2aa53abddc6b449e4ca065892ea5b2ba5394e1
Christina Loiacono <loiaconochristina@gmail.com>
2023-02-19 00:19:06 -0500

    created Signup component for Newsletter with form that posts a users contact info to the backend, which posts the data to mailchimp. Users are stored in mailchimp account; Scss styling for form.

--- frontend/src/Components/newsletter/Newsletter.js
+++ frontend/src/Components/newsletter/Newsletter.js
@@ -4,7 +4,7 @@ import './Newsletter.scss';
 
 import logo from '../../assets/cryptotalk-logo.png';
 
-import NewSubscriberForm from "./NewSubscriberForm";
+import Signup from "../signup/Signup";
 
 const Newsletter = () => {
   return (
@@ -24,17 +24,10 @@ const Newsletter = () => {
         </div>
         <div className="newsletterContainer__newsletter__text">
           <h3 className="newsletterContainer__newsletter__text__infoTitle">CryptoTalk Newsletter</h3>
-          <div className="newsletterContainer__newsletter__text__info1">Don't miss the next ones.</div>
           <div className="newsletterContainer__newsletter__text__info2">Get notified about our next workshops & conferences!</div>
-          <div className="newsletterContainer__newsletter__text__emailSubscribe">
-            <NewSubscriberForm />
-            {/* <input placeholder="Your email" />
-            <button>Subscribe</button> */}
-          </div>
+          <div className="newsletterContainer__newsletter__text__info1">Don't miss the next ones.</div>
+          <Signup />
         </div>
-      </div>
-      <div>
-        
       </div>
     </div>
   );
---
+++ frontend/src/Components/signup/Signup.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import './Signup.scss';
+
+const API = process.env.REACT_APP_API_URL;
+
+// TODO: redirect user to a success/fail page
+// Add Error handling: input must be (3 or more) characters for it to be added to mailchimp for lastName
+const Signup = () => {
+  // set hooks for each input field in form on backend
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  // submitting this form on the frontend posts to the backend, which posts the data to mailchimp. Users are stored in mailchimp account.
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${API}/signup`, { firstName, lastName, email });
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      console.log("firstName, lastName, email:", firstName, lastName, email)
+      // error handling
+      if (!firstName || !lastName || !email){
+        // error - alert user to fill out the form
+        window.alert('unsuccessful')
+      } else {
+        // success - alert user that form was successfully submitted
+        window.alert('successful')
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form className="Signup" action="/signup" onSubmit={handleSubmit}>
+        <div className="inputFields">
+		<input 
+          type="text" 
+          name="firstName" 
+          value={firstName} 
+          onChange={(e) => setFirstName(e.target.value)} 
+          placeholder="First Name"
+		  className="firstName"
+        />
+        <input 
+          type="text" 
+          name="lastName" 
+          value={lastName} 
+          onChange={(e) => setLastName(e.target.value)} 
+          placeholder="Last Name"
+		  className="lastName"
+        />
+        <input 
+          type="email" 
+          name="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email" 
+		  className="email"
+        />
+		</div>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default Signup;
