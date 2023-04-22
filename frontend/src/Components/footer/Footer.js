a6bad929079294573da4159efafeb93a3e410ebb
Christina Loiacono <loiaconochristina@gmail.com>
2023-04-22 16:23:04 -0400

    add max width to footer and newsletter
    Max width for footer is same as navbar

--- frontend/src/Components/footer/Footer.js
+++ frontend/src/Components/footer/Footer.js
@@ -8,55 +8,57 @@ import Newsletter from "../newsletter/Newsletter.js";
 function Footer() {
   return (
     <div className="footer">
-      <Newsletter />
-      <div className="footer__categories">
-        <ul>
-          About Us
-          <li><Link to="/about">About</Link></li>
-          <li>Careers</li>
-          <li>Terms</li>
-          <li>Privacy</li>
-        </ul>
+      <div className="footer__content">
+        <Newsletter />
+        <div className="footer__content__categories">
+          <ul>
+            About Us
+            <li><Link to="/about">About</Link></li>
+            <li>Careers</li>
+            <li>Terms</li>
+            <li>Privacy</li>
+          </ul>
 
-        <ul>
-          Products
-          <li>Exchange</li>
-          <li>Charity</li>
-          <li>Research</li>
-          <li><Link to="/shop">NFT</Link></li>
-        </ul>
+          <ul>
+            Products
+            <li>Exchange</li>
+            <li>Charity</li>
+            <li>Research</li>
+            <li><Link to="/shop">NFT</Link></li>
+          </ul>
 
-        {/* <ul>
-          Service
-          <li>Downloads</li>
-          <li>Buy Crypto</li>
-          <li>Referral</li>
-        </ul> */}
+          {/* <ul>
+            Service
+            <li>Downloads</li>
+            <li>Buy Crypto</li>
+            <li>Referral</li>
+          </ul> */}
 
-        <ul>
-          Support
-          <li>Give Us Feedback</li>
-          <li>Support Center</li>
-          <li>Submit a Request</li>
-          <li>APIs</li>
-        </ul>
+          <ul>
+            Support
+            <li>Give Us Feedback</li>
+            <li>Support Center</li>
+            <li>Submit a Request</li>
+            <li>APIs</li>
+          </ul>
 
-        <ul>
-          Learn
-          <li><Link to="/learn">Learn</Link></li>
-          <li>Browse Crypto Prices</li>
-          <li>Buy Bitcoin</li>
-        </ul>
+          <ul>
+            Learn
+            <li><Link to="/learn">Learn</Link></li>
+            <li>Browse Crypto Prices</li>
+            <li>Buy Bitcoin</li>
+          </ul>
 
-        <ul>
-          Community
-          <li>Twitter</li>
-          <li>TikTok</li>
-          <li>Reddit</li>
-          <li>YouTube</li>
-        </ul>
+          <ul>
+            Community
+            <li>Twitter</li>
+            <li>TikTok</li>
+            <li>Reddit</li>
+            <li>YouTube</li>
+          </ul>
 
-        <div className="footer__categories__copyright">Copyright 2022 CryptoTalk</div>
+          <div className="footer__content__categories__copyright">Copyright 2022 CryptoTalk</div>
+        </div>
       </div>
     </div>
   );
