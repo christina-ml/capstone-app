df9352da38d1089ec802ba5d8f4e0b37e3805516
Christina Loiacono <loiaconochristina@gmail.com>
2023-09-26 18:19:26 -0400

    When a user is logged in - add button for adding a favorite Coin A post request to the favorites table adds a favorite with coin ID and logged in user's user ID

--- frontend/src/Components/allCoins/Coin.js
+++ frontend/src/Components/allCoins/Coin.js
@@ -1,5 +1,10 @@
+import axios from 'axios';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+// User Login information
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
 
 import './Coin.scss';
 
@@ -9,7 +14,35 @@ import addCommas from '../../helpers/AddCommas';
 // chart js for coin changes
 import ChartJsCoin from './chartjs/ChartJsCoin';
 
+// react icons
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+
+const API = process.env.REACT_APP_API_URL;
+
 const Coin = ({coin}) => {
+  // const jwtDecodeUser = jwt_decode(Cookies.get('accessToken'));
+  // console.log(jwtDecodeUser)
+  // console.log(jwtDecodeUser.uid)
+
+  // need to POST to: favorites_uid, favorites_cid
+  const addFavorite = (e) => {        
+    const jwtDecodeUser = jwt_decode(Cookies.get('accessToken'));
+    e.preventDefault();
+
+    // check if accessToken exists in cookies
+    if (Cookies.get('accessToken')){
+      // user is logged in and can favorite this coin
+      axios.post(`${API}/favorites`, {
+        favorites_uid: `${jwtDecodeUser.uid}`,
+        favorites_cid: `${coin.cid}`
+      })
+      .then((res) => {
+
+      }).catch((err) => {
+          console.log(err);
+      })
+    }
+  }
 
   return (
     <div className="coin">
@@ -20,6 +53,12 @@ const Coin = ({coin}) => {
             <div className="coin__card__logo__coinName">
               {coin.name} <span className="coin__card__logo__coinName__symbol">{coin.symbol}</span>
             </div>
+            <button 
+                       className="coin__card__logo__favoriteCoin"
+                        onClick={addFavorite}
+                    >
+                        <FaRegHeart></FaRegHeart>
+                    </button>
           </div>
           <hr />
           <div className="coin__card__basicInfo">
