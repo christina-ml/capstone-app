1664aa91cf1fa9f46e8b643bf23c1fd4b6ed5911
Christina Loiacono <loiaconochristina@gmail.com>
2023-11-08 20:21:58 -0500

    Update favorites controller & query POST and DELETE Test POST and DELETE using Postman Create a GET all favorites, comment out query/controller because it's only for testing in Postman

--- backend/queries/favorites.js
+++ backend/queries/favorites.js
@@ -1,36 +1,48 @@
 const db = require("../db/dbConfig.js");
 
+// get all favorites (to check in Postman)
+// const getAllFavorites = async () => {
+// 	try {
+// 		const allFavorites = await db.any("SELECT * FROM favorites");
+// 		return allFavorites;
+// 	} catch (error) {
+// 		return error;
+// 	}
+// };
+
 // create one favorite
 const createFavorite = async (favorites) => {
-    const {favorites_uid, favorites_cid} = favorites;
-    try {
-        const newFavorite = await db.one(
-            "INSERT INTO favorites (favorites_uid, favorites_cid) VALUES ($1, $2) RETURNING *", 
-            [
-                favorites_uid,
-                favorites_cid
-            ]
-        );
-        return newFavorite;
-    } catch (error){
-        return error;
-    }
-}
+	const { favorites_uid, favorites_cid } = favorites;
+	try {
+		const newFavorite = await db.one(
+			"INSERT INTO favorites (favorites_uid, favorites_cid) VALUES ($1, $2) RETURNING *",
+			[favorites_uid, favorites_cid]
+		);
+		return newFavorite;
+	} catch (error) {
+		console.log("favorite creation err msg:", error);
+		return error;
+	}
+};
 
-// delete one favorite
-const deleteFavorite = async(fid) => {
-    try{
-        const deletedFavorite = await db.one(
-            "DELETE FROM favorites WHERE fid=$1 RETURNING *",
-            fid
-        );
-        return deletedFavorite;
-    } catch(err){
-        return err;
-    }
+// delete one favorite by favorites_uid and favorites_cid
+const deleteFavorite = async (favorites_uid, favorites_cid) => {
+	try {
+		const deletedFavorite = await db.one(
+			`DELETE FROM favorites 
+            WHERE favorites_uid = $1 
+            AND favorites_cid = $2 
+            RETURNING *`,
+			[favorites_uid, favorites_cid]
+		);
+		return deletedFavorite;
+	} catch (err) {
+		return err;
+	}
 };
 
 module.exports = {
-    createFavorite,
-    deleteFavorite,
-}
+	// getAllFavorites,
+	createFavorite,
+	deleteFavorite,
+};
