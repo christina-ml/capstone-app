b2ed38cd8c2cdba80365fd19c491cb41e907c709
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-07 23:05:02 -0500

    Add LoadingSpinner component to CoinList

--- frontend/src/Components/allCoins/CoinList.js
+++ frontend/src/Components/allCoins/CoinList.js
@@ -1,27 +1,58 @@
 import React from 'react';
 import axios from "axios";
 import { useEffect, useState } from "react";
-
-import { ImSpinner2 } from 'react-icons/im';
-
 import Coin from './Coin';
+import LoadingSpinner from '../../cryptotalkComponents/buttons/LoadingSpinner';
+
+// SCSS & react icons
 import './CoinList.scss';
+
+// User Login information
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
 
 const API = process.env.REACT_APP_API_URL;
 
-const CoinList = () => {
+const CoinList = ({ loggedIn }) => {
   const [allCoins, setAllCoins] = useState([]);
+
+  // const jwtDecodeUser = jwt_decode(Cookies.get('accessToken'));
+  // console.log(jwtDecodeUser)
+	// console.log(jwtDecodeUser.uid)
   
+
+  /*
+    if jwtDecodeUser has a value, there is an access token
+    Then I want to fetch from the backend with coins and favorites
+  */
+
+
   // get all coins that exist in the database
   useEffect(() => {
     axios.get(`${API}/coins`)
         .then((res) => {
-            console.log("res.data:", res.data)
+            // console.log("res.data:", res.data)
             setAllCoins(res.data);
         }).catch((err) => {
             console.log(err);
         })
   }, []);
+
+  // useEffect(() => {
+  //   axios.get(`${API}/coins`, {
+  //     headers: {
+  //       authorization: `Bearer ${Cookies.get("accessToken")}`
+  //     }
+  //   })
+  //       .then((res) => {
+  //           console.log("res.data:", res.data)
+  //           setAllCoins(res.data);
+  //       }).catch((err) => {
+  //           console.log(err);
+  //       })
+  // }, []);
+
+  // console.log("allCoins", allCoins)
 
   // make `allCoins` sorted alphabetically
   const sortedCoinsAsc = allCoins.sort((a,b) => {
@@ -32,19 +63,16 @@ const CoinList = () => {
     <div className="coinList">
       <h2>Cryptocurrencies ({allCoins.length})</h2>
       {allCoins.length === 0 ? 
-        <div className="coinList__missingCollection">
-          <div className="coinList__missingCollection__spinner">
-            <ImSpinner2 />
-          </div>
-          <div>
-            Loading...
-          </div>
-        </div> 
+        <LoadingSpinner spinnerLabel="Loading..." />
       :
         <div className="coinList__collection">
           {sortedCoinsAsc.map((coin) => {
             return (
-              <Coin coin={coin} key={coin.name + coin.cid}/>
+              <Coin 
+                coin={coin} 
+                key={coin.name + coin.cid}
+                loggedIn={loggedIn}
+              />
             )
           })}
         </div>
