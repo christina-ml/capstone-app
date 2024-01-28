a6b1f38552a2a620e5181d87d6ea4e58320add70
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-27 21:17:15 -0500

    Add custom color picker for coloring book and SCSS styling

--- frontend/src/Components/coloringBook/NewColorPalette.js
+++ frontend/src/Components/coloringBook/NewColorPalette.js
@@ -1,21 +1,65 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const NewColorPalette = ({currentColor, changeColor}) => {
+const NewColorPalette = ({ currentColor, changeColor }) => {
+	const [customColor, setCustomColor] = useState(currentColor);
 
-    const colors = ['#B22222', 'hotpink', 'orange', '#BDB76B', '#008B8B', '#40E0D0', '#4366F5', '#8599eb', '#1E90FF' , 'grey', 'black', 'white'];
+	const colors = [
+		"#D1345B",
+        "#6DD3CE",
+        "#C8E9A0",
+		"#4366F5",
+		"#1E90FF",
+		"#8599eb",
+        "#F7A278",
+        "#40376E",
+        "#48233C",
+        "#A13D63",
+        "#7798AB",
+        "#C3DBC5",
+        "#E8DCB9",
+        "#F2CEE6",
+		"grey",
+		"#070707",
+		"#FFFFFF",
+        "#EFEFEF"
+	];
 
-    return(
-        <div className="ColoringBook__colorPalette">
-            {colors.map(color => { 
-                const activeClass = currentColor === color ? "ColoringBook__colorSwatchActive" : "";
-                return (
-                    <div onClick={() => {changeColor(color)}}>
-                        <div className={`ColoringBook__colorSwatch ${activeClass}`}  style={{backgroundColor: color}}></div>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+	const handleColorChange = (e) => {
+		e.preventDefault();
+		const newColor = e.target.value;
+		setCustomColor(newColor);
+		changeColor(newColor);
+	};
+
+	return (
+		<div className="ColoringBook__colorPalette">
+			<input
+				className="ColoringBook__colorPalette__customColor"
+				onChange={handleColorChange}
+				id="customColor"
+				type="color"
+				value={customColor}
+			/>
+			{colors.map((color) => {
+				const activeClass =
+					currentColor === color
+						? "ColoringBook__colorSwatchActive"
+						: "";
+				return (
+					<div
+						onClick={() => {
+							changeColor(color);
+						}}
+					>
+						<div
+							className={`ColoringBook__colorSwatch ${activeClass}`}
+							style={{ backgroundColor: color }}
+						></div>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
 export default NewColorPalette;
