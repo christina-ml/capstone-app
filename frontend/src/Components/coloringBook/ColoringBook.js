7d174ff6b534e7c04feeb6f21d6fafe0beedd330
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-28 10:35:57 -0500

    Create button to add custom color to the color palette Add SCSS styling for color chips and button Add labels and information about the coloring page for the user

--- frontend/src/Components/coloringBook/ColoringBook.js
+++ frontend/src/Components/coloringBook/ColoringBook.js
@@ -8,7 +8,7 @@ import NftColoring from "./NftColoring";
 
 const ColoringBook = () => {
   const [fillColors, setFillColors] = useState(Array(30).fill('white'));
-  const [currentColor, setCurrentColor] = useState('white');
+  const [currentColor, setCurrentColor] = useState('black');
 
   const onFillColor = (i) => {
     let newFillColors = fillColors.slice(0);
@@ -20,13 +20,17 @@ const ColoringBook = () => {
     <div className="ColoringBook">
       <div className="ColoringBook__desktop">
       <h1>Crypto Coloring</h1>
+        <span>Color your own NFT!</span>
+        <h4>CryptoTalk Goat #001</h4>
         <NftColoring fillColors={fillColors} onFill={onFillColor}/>
       </div>
       <div className="ColoringBook__mobile">
         <h1>Crypto Coloring</h1>
+        <span>Crypto for mobile!</span>
         <MobileColoring fillColors={fillColors} onFill={onFillColor} />
       </div>
       <NewColorPalette currentColor={currentColor} changeColor={setCurrentColor}/>
+      <h4>Take a screenshot when finished to save your NFT!</h4>
     </div>
   );
 };
--- frontend/src/Components/coloringBook/NewColorPalette.js
+++ frontend/src/Components/coloringBook/NewColorPalette.js
@@ -2,27 +2,13 @@ import React, { useState } from "react";
 
 const NewColorPalette = ({ currentColor, changeColor }) => {
 	const [customColor, setCustomColor] = useState(currentColor);
+	const [colors, setColors] = useState([ "#D1345B", "#6DD3CE", "#C8E9A0", "#4366F5", "#1E90FF", "#8599eb", "#F7A278", "#40376E", "#48233C", "#A13D63", "#7798AB", "#C3DBC5", "#E8DCB9", "#F2CEE6", "grey", "#070707", "#FFFFFF", "#EFEFEF"]);
 
-	const colors = [
-		"#D1345B",
-        "#6DD3CE",
-        "#C8E9A0",
-		"#4366F5",
-		"#1E90FF",
-		"#8599eb",
-        "#F7A278",
-        "#40376E",
-        "#48233C",
-        "#A13D63",
-        "#7798AB",
-        "#C3DBC5",
-        "#E8DCB9",
-        "#F2CEE6",
-		"grey",
-		"#070707",
-		"#FFFFFF",
-        "#EFEFEF"
-	];
+	const handleAddCustomColorToPalette = (e) => {
+		e.preventDefault();
+		const newColor = customColor;
+		setColors([...colors, newColor]);
+	}
 
 	const handleColorChange = (e) => {
 		e.preventDefault();
@@ -33,13 +19,24 @@ const NewColorPalette = ({ currentColor, changeColor }) => {
 
 	return (
 		<div className="ColoringBook__colorPalette">
-			<input
-				className="ColoringBook__colorPalette__customColor"
-				onChange={handleColorChange}
-				id="customColor"
-				type="color"
-				value={customColor}
-			/>
+			<div className="ColoringBook__colorPalette__custonColor">
+				<input
+					className="ColoringBook__colorPalette__customColor__newColor"
+					onChange={handleColorChange}
+					id="customColor"
+					type="color"
+					value={customColor}
+				/>
+				<div>
+					<button 
+						className="ColoringBook__colorPalette__customColor__addToPalette"
+						type="button"  
+						onClick={handleAddCustomColorToPalette}
+					>
+						Add to palette
+					</button>
+				</div>
+			</div>
 			{colors.map((color) => {
 				const activeClass =
 					currentColor === color
