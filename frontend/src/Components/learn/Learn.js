4737da82adc69996853aed008849c0c5f533e22e
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-30 03:38:48 -0600

    Add conditional rendering if no results on Learn page

--- frontend/src/Components/learn/Learn.js
+++ frontend/src/Components/learn/Learn.js
@@ -1,59 +1,59 @@
-// Resources for questions:
-// https://learncrypto.com/tldr/crypto-basics#who-is-satoshi-nakamoto-why-are-they-important
-// https://learncrypto.com/blog
-// https://learncrypto.com/knowledge-base/basics
-// https://www.coinbase.com/learn/crypto-basics
-
-import React, { useState } from 'react';
-import "./Learn.scss";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 // questions and answers
-import learnData from './learnData';
-import Accordion from './Accordion';
+import learnData from "./learnData";
 
-import SearchBar from '../searchBar/SearchBar';
+// Components
+import Accordion from "./Accordion";
+import SearchBar from "../searchBar/SearchBar";
+
+import "./Learn.scss";
 
 const Learn = () => {
-  const [searchTerm, setSearchTerm] = useState(''); // for searchBar
+	const [searchTerm, setSearchTerm] = useState(""); // for searchBar
 
-  // search bar - filter by topics
-  let filteredTopics = learnData;
-  if (searchTerm){
-      filteredTopics = learnData.filter(learnDataQandA => {
-          const topic = `${learnDataQandA.topic} ${learnDataQandA.question}`;
-          const topicToLowerCase = topic.toLowerCase();
-          const searchTermToLowerCase = searchTerm.toLowerCase();
-          return topicToLowerCase.includes(searchTermToLowerCase);
-      })
-  }
+	// search bar - filter by topics
+	let filteredTopics = learnData;
+	if (searchTerm) {
+		filteredTopics = learnData.filter((learnDataQandA) => {
+			const topic = `${learnDataQandA.topic} ${learnDataQandA.question}`;
+			const topicToLowerCase = topic.toLowerCase();
+			const searchTermToLowerCase = searchTerm.toLowerCase();
+			return topicToLowerCase.includes(searchTermToLowerCase);
+		});
+	}
 
-  // questions card using accordion
-  let learnQuestionsAccordion = filteredTopics.map((learn) => {
-    return (
-      <Accordion learn={learn} />
-    )
-  })
+	// questions card using accordion
+	let learnQuestionsAccordion = filteredTopics.map((learn, index) => {
+		return <Accordion learn={learn} key={index + uuidv4()} />;
+	});
 
-  return (
-    <div className="Learn">
-        <header>
-            <h1>Learn CryptoTalk</h1>
-        </header>
-        <div className="Learn__startHere">
-          <h3>Crypto basics</h3>
-          <div>New to crypto? — start here</div>
-        </div>
-        <div className="Learn__search">
-          <SearchBar searchTerm={searchTerm} 
-                    setSearchTerm={setSearchTerm} 
-                    placeholder={`Search by topic or question`}
-          />
-        </div>
-        <div className="Learn__questionsAccordion">
-          {learnQuestionsAccordion}
-        </div>
-    </div>
-  )
-}
+	return (
+		<div className="Learn">
+			<header>
+				<h1>Learn CryptoTalk</h1>
+			</header>
+			<div className="Learn__startHere">
+				<h3>Crypto basics</h3>
+				<div>New to crypto? — start here</div>
+			</div>
+			<div className="Learn__search">
+				<SearchBar
+					searchTerm={searchTerm}
+					setSearchTerm={setSearchTerm}
+					placeholder={`Search by topic or question`}
+				/>
+			</div>
+			{filteredTopics.length ? (
+				<div className="Learn__questionsAccordion">
+					{learnQuestionsAccordion}
+				</div>
+			) : (
+				<div>No results: Topic or question not found.</div>
+			)}
+		</div>
+	);
+};
 
 export default Learn;
