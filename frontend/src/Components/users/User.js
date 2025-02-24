3bd2440e5e4aaee4f42e751215fc72d326788460
Christina Loiacono <loiaconochristina@gmail.com>
2025-02-24 15:41:19 -0500

    Fix padding and displayed info for User

--- frontend/src/Components/users/User.js
+++ frontend/src/Components/users/User.js
@@ -1,57 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { IoSettingsSharp } from 'react-icons/io5';
+import { IoSettingsSharp } from "react-icons/io5";
 
-import './User.scss';
+import "./User.scss";
 
-const User = ({user}) => {
+const User = ({ user }) => {
+	const {
+		uid,
+		firstname,
+		lastname,
+		username,
+		photo,
+		user_admin,
+		// user_password,
+		// user_email,
+		// user_active,
+		// user_interests,
+		user_city,
+		user_state,
+	} = user;
 
-  const { uid,
-    firstname,
-    lastname,
-    username,
-    photo,
-    user_admin,
-    // user_password,
-    // user_email,
-    // user_active,
-    // user_interests,
-    // user_city,
-    // user_state 
-  } = user;
-
-  return (
-    <div className="userCard">
-      <div className="userCard__profileImage">
-        <Link to={`/users/${uid}`} >
-          <img src={photo} alt="user profile avatar" />
-        </Link>
-      </div>
-      <div className="userCard__info">
-        <div className="userCard__info__name">
-          <h1>{(firstname || lastname) ? `${firstname} ${lastname || ''}` : `${username}`}
-            {user_admin ? 
-            <div className="userCard__info__name__tooltip">
-              <div className="userCard__info__name__tooltip__tooltiptext">
-                <IoSettingsSharp />
-              </div>
-            </div>
-            :
-             <span></span>}
-          </h1>
-        </div>
-        <div className="userCard__info__infoLine">
-          {username}
-        </div>
-        <div className="userCard__info__infoLine__button">
-          <Link to={`/users/${uid}`} >
-            See User Details
-          </Link>
-        </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="userCard">
+			<div className="userCard__profileImage">
+				<Link to={`/users/${uid}`}>
+					<img src={photo} alt="user profile avatar" />
+				</Link>
+			</div>
+			<div className="userCard__info">
+				<div className="userCard__info__name">
+					<h1>
+						{firstname || lastname
+							? `${firstname} ${lastname || ""}`
+							: `${username}`}
+						{user_admin ? (
+							<div className="userCard__info__name__tooltip">
+								<div className="userCard__info__name__tooltip__tooltiptext">
+									<IoSettingsSharp />
+								</div>
+							</div>
+						) : (
+							<span></span>
+						)}
+					</h1>
+				</div>
+				<div className="userCard__info__infoLine">
+					{user_city}, {user_state}
+				</div>
+				<div className="userCard__info__infoLine__button">
+					<Link to={`/users/${uid}`}>See User Details</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default User;
